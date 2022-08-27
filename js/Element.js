cb1bee54114fec59451ff97280d68b8b22017cdd
vvye <ekaiser.hellwege@gmail.com>
2022-08-27 14:03:13 +0200

    Implement element

---
+++ js/Element.js
@@ -0,0 +1,33 @@
+class Element {
+
+    x;
+    y;
+    lifetime;
+    decaySpeed;
+    dead;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.lifetime = 255;
+        this.decaySpeed = 1;
+        this.dead = false;
+    }
+
+    update() {
+        this.lifetime -= this.decaySpeed;
+        if (this.lifetime <= 0) {
+            this.die();
+        }
+    }
+
+    draw() {
+        fill(102, 153, 204, this.lifetime);
+        circle(this.x, this.y, 20);
+    }
+
+    die() {
+        this.dead = true;
+    }
+
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,17 @@
+let elements = [];
+
 function setup() {
     let canvas = createCanvas(400, 400);
     canvas.parent('main-section');
+
+    elements.push(new Element(200, 200));
 }
 
 function draw() {
     background(0);
+
+    for (let elem of elements) {
+        elem.update();
+        elem.draw();
+    }
 }
